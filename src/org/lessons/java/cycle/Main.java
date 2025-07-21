9d3326c73dec313a281eef2f35cd8b5c68da7167
lorenzosalti <lorenzosalti@gmail.com>
2025-07-21 16:36:16 +0200

    feata: cycle tests

--- src/org/lessons/java/cycle/Main.java
+++ src/org/lessons/java/cycle/Main.java
@@ -3,5 +3,26 @@ package org.lessons.java.cycle;
 public class Main {
   public static void main(String[] args) {
 
+    int[] daUnoADieci = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    PistaCiclabile daCiclare = new PistaCiclabile(daUnoADieci);
+
+    System.out.println(daCiclare.hasAncoraElementi()); // true
+    System.out.println(daCiclare.getElementoSuccessivo()); // 1
+    System.out.println(daCiclare.getElementoSuccessivo()); // 2
+    System.out.println(daCiclare.getElementoSuccessivo()); // 3
+    System.out.println(daCiclare.hasAncoraElementi()); // true
+    System.out.println(daCiclare.getElementoSuccessivo()); // 4
+    System.out.println(daCiclare.getElementoSuccessivo()); // 5
+    System.out.println(daCiclare.hasAncoraElementi()); // true
+    System.out.println(daCiclare.getElementoSuccessivo()); // 6
+    System.out.println(daCiclare.getElementoSuccessivo()); // 7
+    System.out.println(daCiclare.hasAncoraElementi()); // true
+    System.out.println(daCiclare.getElementoSuccessivo()); // 8
+    System.out.println(daCiclare.getElementoSuccessivo()); // 9
+    System.out.println(daCiclare.hasAncoraElementi()); // true
+    System.out.println(daCiclare.getElementoSuccessivo()); // 10
+    System.out.println(daCiclare.hasAncoraElementi()); // false
+
   }
 }
