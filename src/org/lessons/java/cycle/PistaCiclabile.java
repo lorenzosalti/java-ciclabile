61ab70d0467984e26e70e2e8ccfb2c3e0d7c5fba
lorenzosalti <lorenzosalti@gmail.com>
2025-07-21 16:53:56 +0200

    feat: bonus task

--- src/org/lessons/java/cycle/PistaCiclabile.java
+++ src/org/lessons/java/cycle/PistaCiclabile.java
@@ -21,4 +21,30 @@ public class PistaCiclabile {
   public boolean hasAncoraElementi() {
     return currentElementIndex < this.elements.length;
   }
+
+  // BONUS
+  public PistaCiclabile() {
+  }
+
+  public void addElemento(int elementToAdd) {
+
+    int[] newElements = new int[this.elements.length + 1];
+
+    for (int i = 0; i < this.elements.length; i++) {
+      int currentElement = this.elements[i];
+      newElements[i] = currentElement;
+    }
+
+    newElements[newElements.length - 1] = elementToAdd;
+
+    this.elements = newElements;
+  }
+
+  /*
+   * creare un nuovo array di lunghezza pari a quello attuale + 1
+   * ciclare sul vecchio array per reinserire tutti i vecchi elementi in quello
+   * nuovo
+   * finito il ciclco aggiungere l'elemento passato come parametro
+   * sovrascrivere il vecchio array con il nuovo
+   */
 }
